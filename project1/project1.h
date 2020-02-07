1b09932325a4188c153c21b54fbb22baa20cf56f
Shafee Mustafa <mustaf14@egr.msu.edu>
2020-02-06 19:44:47 -0500

    Add project files.

---
+++ project1/project1.h
@@ -0,0 +1,39 @@
+
+// project1.h : main header file for the project1 application
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // main symbols
+
+
+// Cproject1App:
+// See project1.cpp for the implementation of this class
+//
+
+class Cproject1App : public CWinApp
+{
+public:
+	Cproject1App() noexcept;
+
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// Implementation
+protected:
+	HMENU  m_hMDIMenu;
+	HACCEL m_hMDIAccel;
+
+public:
+	afx_msg void OnAppAbout();
+	afx_msg void OnFileNew();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern Cproject1App theApp;
