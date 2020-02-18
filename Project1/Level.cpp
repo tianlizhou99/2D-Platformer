195a8366cb0fd807e4074b16e14da5a57bdcbc39
moore146 <moore146@msu.edu>
2020-02-18 16:17:52 -0500

    small changes that i can't undo for some fun reason

--- Project1/Level.cpp
+++ Project1/Level.cpp
@@ -24,6 +24,4 @@ CLevel::CLevel(CGame* game, const std::wstring& filename) :
 void CLevel::XmlLoad(const std::shared_ptr<xmlnode::CXmlNode>& node)
 {
     CEntity::XmlLoad(node);
-    mWidth = node->GetAttributeDoubleValue(L"width", 0);
-    mHeight = node->GetAttributeDoubleValue(L"height", 0);
 }
--- Project1/Level.h
+++ Project1/Level.h
@@ -37,8 +37,5 @@ private:
 	/// The image of this level component
 	std::unique_ptr<Gdiplus::Bitmap> mImage;
 
-    double mWidth = 0; ///< width of level
-    double mHeight = 0; ///< height of level
-
 };
 
