d2b7cdd6ba0857d19f86bb38b106a5ad56ad115a
Dakota Tyrrell <tyrrel15@msu.edu>
2020-03-08 21:54:32 -0400

    Minor change to GameState

--- project1/Game.h
+++ project1/Game.h
@@ -26,7 +26,7 @@ class CGame
 {
 public:
     ///possible game states
-    enum GameState { start = 0, progress = 1, end = 2, loss = 3, money = 4 };
+    enum GameState { start = 0, progress = 1, end = 2, loss = 3};
 private:
     int mScore = 0; ///< score of the player
     GameState mGameState = start; ///< current state of the game
