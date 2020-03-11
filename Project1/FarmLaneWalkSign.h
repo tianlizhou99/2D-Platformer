1fe23d5a4cd110ff91517edcdc56bc814d12aeed
moore146 <moore146@msu.edu>
2020-03-10 21:23:09 -0400

    small comment changes

---
+++ Project1/FarmLaneWalkSign.h
@@ -0,0 +1,51 @@
+/**
+ * \file FarmLaneWalkSign.h
+ *
+ * \author Team Sprixie
+ *
+ * Class that represents a Farm Lane walk sign
+ */
+
+#pragma once
+#include "Level.h"
+#include "Entity.h"
+
+/**
+* Class for Farm Lane Walk Sign item
+*/
+class CFarmLaneWalkSign :
+	public CLevel
+{
+public:
+	CFarmLaneWalkSign(CGame* game);
+
+	/// Default constructor (disabled)
+	CFarmLaneWalkSign() = delete;
+
+	/// Copy constructor (disabled)
+	CFarmLaneWalkSign(const CFarmLaneWalkSign&) = delete;
+
+	CFarmLaneWalkSign(CGame* game, wstring& filename);
+
+	///possible sign states
+	enum Sign {stop = 0, walk = 1};
+
+	/** Gets the sign state
+	 * \returns Sign state */
+	Sign GetSign() { return mSign; }
+
+	/** Sets the sign according to the time that has elapsed
+	 * \param elapse Time elapsed since last call
+	 * \returns void */
+	virtual void Update(double elapse);
+
+	void Draw(Gdiplus::Graphics* graphics) override;
+
+	virtual void Collision(CPlayer* player) override;
+
+private:
+	double mElapsed = 0; ///<Time elapsed since last call
+	Sign mSign = stop; ///<Sign state
+	double mMultiplier = 2; ///< Speed multiplier
+};
+
